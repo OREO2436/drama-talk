43b74b7184028348a306145d67e6ea4a28caf15c
ORE02436 <2027506537@qq.com>
2026-04-10 15:48:52 +0800

    fix: 删除basePath，适配Vercel根路径部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 开启静态导出，GitHub Pages 必须配置
-  output: 'export',
-  // 设置 basePath，必须和你的仓库名完全一致！
-  basePath: '/drama-talk',
-  // 禁用 Next.js 图片优化，静态导出不支持该功能
   images: {
     unoptimized: true,
   },
